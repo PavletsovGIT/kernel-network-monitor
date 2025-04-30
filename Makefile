da3ca1c17cb5e6a9cb177fba517f0a59da50a502
Feodor Pavletsov <pavletsovgames@gmail.com>
2025-04-30 12:22:24 +0700

    Fixed common/ioctl and makefiles

--- Makefile
+++ Makefile
@@ -1,4 +1,27 @@
-# Подключаем make-файл модуля ядра
-include ./kern-module/Makefile_km
-# Подключаем make-файл пользоват. приложения
-# include ./userspace/Makefile_us
+CC = gcc
+CFLAGS = -Wall -ansi -pedantic -std=c99
+export CFLAGS
+
+all: kernel_module userspace_apps test_apps
+
+kernel_module:
+	$(MAKE) -C kern-module -f Makefile
+
+userspace_apps:
+	$(MAKE) -C userspace -f Makefile
+
+test_apps:
+	$(MAKE) -C tests -f Makefile
+
+clean:
+	$(MAKE) -C kern-module -f Makefile clean_km
+	$(MAKE) -C userspace -f Makefile clean
+	$(MAKE) -C tests -f Makefile clean
+
+load:
+	sudo insmod kern-module/mynetmod.ko
+
+unload:
+	sudo rmmod mynetmod
+
+.PHONY: all kernel_module userspace_apps test_apps clean load unload
--- kern-module/mynetmod.h
+++ kern-module/mynetmod.h
@@ -13,13 +13,53 @@
 #include <linux/inet.h>
 #include <linux/proc_fs.h>
 
+#define NAME_SIZE 64
+
+#define BUF_SIZE 1024
+
+#define SRC_IP_BIT_MASK 	0x1		// 00000001
+#define DST_IP_BIT_MASK 	0x2		// 00000010
+#define SRC_PORT_BIT_MASK 	0x4		// 00000100
+#define DST_PORT_BIT_MASK 	0x8		// 00001000
+#define PROTO_BIT_MASK 		0x10	// 00010000
+
+/* defined_fields - переменная для указания какие поля в kern_rule заполнены
+ * 0 бит - src_ip
+ * 1 бит - dst_ip
+ * 2 бит - src_port
+ * 3 бит - dst_port
+ * 4 бит - proto
+ * 5 бит - Резерв
+ * 6 бит - Резерв
+ * 7 бит - Резерв
+ */
+struct kern_rule {
+	struct in_addr src_ip;
+	struct in_addr dst_ip;
+	uint16_t src_port;
+	uint16_t dst_port;
+	uint8_t proto;
+	uint8_t defined_fields;
+	uint32_t blocking_count; 
+};
+
+struct kern_cmd {
+	struct kern_rule rule;
+	uint8_t res;
+};
+
+// Добавление правила в фильтр
+#define ADD_RULE _IOWR('a', 'a', struct kern_cmd *)
+// Удаление правила из фильтра
+#define DEL_RULE _IOWR('a', 'b', struct kern_cmd *)
+// Чтение стсатистики из ядра
+//#define SHOW _IOR('a', 'c', char *)
+
 #define DEVICE_NAME "mynetmod"
 #define PROC_STATS_NAME "bl_stats"
 
 static struct nf_hook_ops *nf_tracer_ops = NULL;
 static struct nf_hook_ops *nf_tracer_out_ops = NULL;
-
-#include "/home/pavletsov21/eltex/knm/common/ioctl_cmd.h"
 
 /* Список ошибок */
 enum {
